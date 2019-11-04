1f5ee3d9fc696882938c5551ef2bd1a8fb9a89f8
Diego Matienzo <diegomc365@hotmail.com>
2019-11-03 22:23:50 -0500

    add name title and date now in index

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function HelloWorld(name, date){
+  var presentation =(
+    <div>
+      <h2>Hello, I'm {name}</h2>
+      <h3>Today is {date}</h3>
+    </div>
+  );
+  return presentation;
+}
+
 function App() {
+  var name = "Diego Matienzo";
+  // var presentation = <h2>Hello, I'm {name} </h2>
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Project React-Start
         </p>
+        { HelloWorld(name, Date(Date.now())) }
         <a
           className="App-link"
           href="https://reactjs.org"
